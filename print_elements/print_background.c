956b0563d530c9253c8a5d1b06df6d557a1f2e79
palomaprates <paloma.prates108@gmail.com>
2025-02-19 11:45:43 -0300

    fix print_background function

--- print_elements/print_background.c
+++ print_elements/print_background.c
@@ -1,6 +1,6 @@
 #include "../so_long.h"
 
-void print_background()
+void print_background(t_image *canva)
 {
 	int j;
 	int i;
@@ -10,7 +10,7 @@ void print_background()
 		i = 0;
 		while (i < 920)
 		 {
-			my_mlx_pixel_put(&canva, i, j, 0x00FFFFFF);
+			my_mlx_pixel_put(canva, i, j, 0x00FFFFFF);
 			i++;
 		}
 		j++;
