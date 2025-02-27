158fb375e7c3bb8c5027bc2ab1e65ee707c6742b
palomaprates <paloma.prates108@gmail.com>
2025-02-27 09:02:59 -0300

    add print functions

---
+++ print/print_element.c
@@ -0,0 +1,22 @@
+#include "../so_long.h"
+
+void	print_element(t_image *canva, char **map, char c, t_image *img)
+{
+	int	line; 
+	int	length;
+
+	line = 0;
+	while(map[line])
+	{
+		length = 0;
+		while(map[line][length])
+		{
+			if (map[line][length] == c)
+				print_image(canva, img, (length * 20), (line * 20));
+			length++;
+		}
+		line++;
+	}
+}
+
+
