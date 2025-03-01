935ee04c8e5ecede20127e221defb895869540a2
palomaprates <paloma.prates108@gmail.com>
2025-03-01 10:35:59 -0300

    update print_avatar function

--- print/print_avatar.c
+++ print/print_avatar.c
@@ -1,9 +1,10 @@
 #include "../so_long.h"
 
-	void	print_avatar(t_image *canva, t_image player, t_sprite avatar)
+	void	print_avatar(t_image *canva, t_sprite player)
 {
 	int	index_x;
 	int	index_y;
+	int	pixel;
 
 	index_y = 0;
 	while (index_y < 32)
@@ -11,13 +12,12 @@
 		index_x = 0;
 		while (index_x < 32)
 		{
-			if (get_pixel_canva(&player, avatar.width + index_x,
-					avatar.height + index_y) != -16777216)
+			pixel = get_pixel_canva(&player.image, player.width + index_x,
+				player.height + index_y);
+			if (pixel != -16777216 && pixel != 0)
 			{
-				my_mlx_pixel_put(canva, (avatar.x + index_x),
-					(avatar.y + index_y),
-					get_pixel_canva(&player, avatar.width + index_x,
-						avatar.height + index_y));
+				my_mlx_pixel_put(canva, (player.x + index_x),
+					(player.y + index_y), pixel);
 			}
 			index_x++;
 		}
