d833f1817d3f34598a63b21e2743e206ca0ac9b3
palomaprates <paloma.prates108@gmail.com>
2025-03-01 10:35:23 -0300

    add and test exit function

--- keys/keys_hook.c
+++ keys/keys_hook.c
@@ -4,46 +4,66 @@ int	keys_hook(int key_code, t_data *param)
 {
 	if (key_code == 115)
 	{
-		param->avatar.y += 10;
-		param->avatar.height = 98;
-		if (param->avatar.width >= 32)
-			param->avatar.width = 1;
+		param->player.y += 10;
+		param->player.height = 98;
+		if (param->player.width >= 32)
+			param->player.width = 1;
 	}
 	if (key_code == 119)
 	{
-		param->avatar.y -= 10;
-		param->avatar.height = 66;
-		param->avatar.width += 33;
-		if (param->avatar.width >= 60)
-			param->avatar.width = 1;
+		param->player.y -= 10;
+		param->player.height = 66;
+		param->player.width += 33;
+		if (param->player.width >= 60)
+			param->player.width = 1;
 		
 	}
 	if (key_code == 97)
 	{
-		param->avatar.x -= 10;
-		param->avatar.height = 34;
-		param->avatar.width += 33;
-		if (param->avatar.width >= 60)
-			param->avatar.width = 1;
+		param->player.x -= 10;
+		param->player.height = 34;
+		param->player.width += 33;
+		if (param->player.width >= 60)
+			param->player.width = 1;
 	}
 	if (key_code == 100)
 	{
-		param->avatar.x += 10;
-		param->avatar.height = 1;
-		param->avatar.width += 33;
-		if (param->avatar.width >= 60)
-			param->avatar.width = 1;
+		param->player.x += 10;
+		param->player.height = 1;
+		param->player.width += 33;
+		if (param->player.width >= 60)
+			param->player.width = 1;
 	}
-	// print_image(&param->canva, &param->player, param->avatar.x, param->avatar.y);
+	
 	print_element(&param->canva, param->map, BACKGROUND, &param->background);
 	print_element(&param->canva, param->map, EXIT, &param->background);
 	print_element(&param->canva, param->map, PLAYER, &param->background);
 	print_element(&param->canva, param->map, COINS, &param->background);
 	print_element(&param->canva, param->map, WALL, &param->wall);
-	print_avatar(&param->canva, param->player, param->avatar);
-	// print_element(&param->canva, param->map, COINS, &param->wall);
-	// print_walls(&param->canva, param->wall, param->avatar, param->map);
-
+	print_element(&param->canva, param->map, COINS, &param->coins);
+	
+	if (key_code == 100)
+	{
+		printf("entrou\n");
+		int i = 0;
+		param->exit.width = -32;
+		while (i < 4)
+		{
+			param->exit.x = 15;
+			param->exit.y = 10;
+			param->exit.width += 33;
+			if (param->exit.width >= 133)
+			param->exit.width = 1;
+			param->exit.height = 1;
+			print_element(&param->canva, param->map, EXIT, &param->background);
+			print_avatar(&param->canva, param->exit);
+			print_avatar(&param->canva, param->player);
+			mlx_put_image_to_window(param->mlx, param->mlx_win, param->canva.img, 0, 0);
+			usleep(100000);
+			i++;
+		}
+	}
+	print_avatar(&param->canva, param->player);
 	mlx_put_image_to_window(param->mlx, param->mlx_win, param->canva.img, 0, 0);
 	if (key_code == 65307)
 		close_win(param);
