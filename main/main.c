7b0b20ec834e364b002f21d36f41381684b9dc32
palomaprates <paloma.prates108@gmail.com>
2025-03-01 10:37:29 -0300

    att main func

--- main/main.c
+++ main/main.c
@@ -11,9 +11,9 @@ int	main(int argc, char *argv[])
 {
 	t_data	data;
 	int fd = open("map.ber", O_RDONLY);
-	data.map = get_lines_map(fd);
 	int height_win;
 	int width_win;
+	data.map = get_lines_map(fd);
 	if (!parsing_map(data.map))
 		return (0);
 	height_win = get_amount_lines(data.map) * 20;
@@ -21,10 +21,14 @@ int	main(int argc, char *argv[])
 	data.mlx = mlx_init();
 	data.mlx_win = mlx_new_window(data.mlx, width_win, height_win, "So Long");
 	init_images(&data);
-	data.avatar.x = width_win / 2;
-	data.avatar.y = height_win / 2;
-	data.avatar.width = 1;
-	data.avatar.height = 1;
+	data.player.x = width_win / 2;
+	data.player.y = height_win / 2;
+	data.player.width = 1;
+	data.player.height = 1;
+	data.exit.x = 2 * 20;
+	data.exit.y = 2 * 20;
+	data.exit.width = 1;
+	data.exit.height = 1;
 	mlx_put_image_to_window(data.mlx, data.mlx_win, data.canva.img, 0, 0);
 	mlx_hook(data.mlx_win, ON_KEYDOWN, 1L<<0, keys_hook, &data);
 	mlx_hook(data.mlx_win, ON_DESTROY, 1L<<2, destroy_hook, &data);
