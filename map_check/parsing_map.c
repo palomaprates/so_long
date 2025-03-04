26ee7dd5280349138988fd98d4e1c209eb76a527
palomaprates <paloma.prates108@gmail.com>
2025-03-04 20:15:31 -0300

    fix return

--- map_check/parsing_map.c
+++ map_check/parsing_map.c
@@ -7,24 +7,12 @@ int	parsing_map(char **lines_map)
 	if (!*lines_map)
 		return (0);
 	if (!is_map_enclosed(lines_map))
-	{
-		printf("mapa não está fechado");
-		return (0);
-	}
+		return (printf("mapa não está fechado"));
 	if (number_of_elements(lines_map, 'E') != 1)
-	{
-		printf("há mais de uma saída");
-		return (0);
-	}
+		return (printf("há mais de uma saída"));
 	if (number_of_elements(lines_map, 'P') != 1)
-	{
-		printf("há mais de uma posição inicial");
-		return (0);
-	}
+		return (printf("há mais de uma posição inicial"));
 	if (number_of_elements(lines_map, 'C') < 1)
-	{
-		printf("não há coletáveis");
-		return (0);
-	}
+		return (printf("não há coletáveis"));
 	return (1);
 }
