50dc12436edae98d0440cc72b1a220e12ed863a6
palomaprates <paloma.prates108@gmail.com>
2025-03-04 12:33:56 -0300

    pass map to argv

--- main/main.c
+++ main/main.c
@@ -10,7 +10,7 @@ int	destroy_hook(t_data *data)
 int	main(int argc, char *argv[])
 {
 	t_data	data;
-	int fd = open("map.ber", O_RDONLY);
+	int fd = open(argv[1], O_RDONLY);
 	int height_win;
 	int width_win;
 	data.map = get_lines_map(fd);
@@ -29,7 +29,6 @@ int	main(int argc, char *argv[])
 	data.exit.y = 2 * 20;
 	data.exit.width = 1;
 	data.exit.height = 1;
-	data.limit = 0;
 	mlx_put_image_to_window(data.mlx, data.mlx_win, data.canva.img, 0, 0);
 	mlx_hook(data.mlx_win, ON_KEYDOWN, 1L<<0, keys_hook, &data);
 	mlx_hook(data.mlx_win, ON_DESTROY, 1L<<2, destroy_hook, &data);
