8e9586948c4b22c6d15c136bf5addbbdda1a441c
palomaprates <paloma.prates108@gmail.com>
2025-03-13 12:28:54 -0300

    change order

--- game_control/game_images.c
+++ game_control/game_images.c
@@ -2,8 +2,8 @@
 
 void	game_images(t_data *param)
 {
-	char *next_pos;
-	
+	char	*next_pos;
+
 	print_element(&param->canva, param->map, BACKGROUND, &param->background);
 	print_element(&param->canva, param->map, EXIT, &param->background);
 	print_element(&param->canva, param->map, PLAYER, &param->background);
@@ -12,9 +12,9 @@ void	game_images(t_data *param)
 	next_pos = touch_element(param->player.x, param->player.y, param->map);
 	if (*next_pos == COINS)
 		*next_pos = BACKGROUND;
+	print_exit(param);
 	print_avatar(&param->canva, param->player);
 	print_element(&param->canva, param->map, WALL, &param->wall);
-	print_exit(param);
 	if (*next_pos == EXIT && !number_of_elements(param->map, COINS))
 			exit(1);
 	mlx_put_image_to_window(param->mlx, param->mlx_win, param->canva.img, 0, 0);
