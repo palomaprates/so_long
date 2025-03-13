f10f6c742dcb537db60ae8b1bdf5608e9e67ccce
palomaprates <paloma.prates108@gmail.com>
2025-03-13 12:29:06 -0300

    print moves

--- game_control/keys_hook.c
+++ game_control/keys_hook.c
@@ -2,9 +2,11 @@
 
 int	keys_hook(int key_code, t_data *param)
 {
+	static int	i;
 	movements(key_code, param);
+	printf("%d moves\n", i++);
 	game_images(param);
 	if (key_code == 65307)
-		close_win(param);
+	close_win(param);
 	return (0);
 }
