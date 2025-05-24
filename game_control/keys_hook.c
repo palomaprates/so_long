968fab290e3c84835864fdf9030a464685f3139c
palomaprates <paloma.prates108@gmail.com>
2025-05-24 09:39:17 +0100

    fix moves

--- game_control/keys_hook.c
+++ game_control/keys_hook.c
@@ -2,9 +2,10 @@
 
 int	keys_hook(int key_code, t_data *param)
 {
-	// static int	i;
+	static int	i;
 	movements(key_code, param);
-	// ft_printf("%d moves\n", i++);
+	if (key_code == 119 || key_code == 115 || key_code == 97 || key_code == 100)
+		printf("%d moves\n", i++);
 	game_images(param);
 	if (key_code == 65307)
 	close_win(param);
